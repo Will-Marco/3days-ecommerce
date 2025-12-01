077c95b264af8739196b1784c5411cfef86fe42a
Will-Marco <mr.marco0101@gmail.com>
2025-12-01 17:45:57 +0500

    add Shadcn UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black text-black">
       Hello, Next.js!
+      <Button className="ml-4">Click Me</Button>
     </div>
   );
 }
